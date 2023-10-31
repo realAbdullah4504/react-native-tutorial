cfc6dfb67034446a80d52515b9d50813b60331b2
Abdullah Javed <abdullah.javed@nutrifactor.local>
2023-10-31 17:29:04 +0500

    drawer navigation

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
  * @format
  */
 
+// import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
+
 import App from './src/App';
+import 'react-native-reanimated';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,78 @@
-import React, {useState} from 'react';
-import {
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  Button,
-  ScrollView,
-  SafeAreaView,
-  TouchableOpacity,
-  ImageSourcePropType,
-  Image,
-  Pressable,
-  StatusBar,
-  FlatList,
-} from 'react-native';
-
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Feed from './components/Feed';
+import Article from './components/Article';
 
-import Home from './screens/Home';
-import Details from './screens/Details';
+const Drawer = createDrawerNavigator();
 
-export type RootStackParamList = {
-  Home: undefined;
-  Details: {product: Product};
-};
-const stack = createNativeStackNavigator<RootStackParamList>();
-
-const App = (): JSX.Element => {
+const App = () => {
   return (
-    <>
-      {/* <View>
-        <Text>Hello</Text>
-      </View> */}
-      <NavigationContainer>
-        <stack.Navigator>
-          <stack.Screen
-            name="Home"
-            component={Home}
-            options={{title: 'Trending Products'}}
-          />
-          <stack.Screen
-            name="Details"
-            component={Details}
-            options={{title: 'Product Details'}}
-          />
-        </stack.Navigator>
-      </NavigationContainer>
-    </>
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Feed" component={Feed} />
+        <Drawer.Screen name="Article" component={Article} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+    // <View>
+    //   <Text>Hello</Text>
+    // </View>
   );
 };
-
-const styles = StyleSheet.create({});
-
 export default App;
+
+// import React, {useState} from 'react';
+// import {
+//   StyleSheet,
+//   Text,
+//   TextInput,
+//   View,
+//   Button,
+//   ScrollView,
+//   SafeAreaView,
+//   TouchableOpacity,
+//   ImageSourcePropType,
+//   Image,
+//   Pressable,
+//   StatusBar,
+//   FlatList,
+// } from 'react-native';
+
+// import {NavigationContainer} from '@react-navigation/native';
+// import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+// import Home from './screens/Home';
+// import Details from './screens/Details';
+
+// export type RootStackParamList = {
+//   Home: undefined;
+//   Details: {product: Product};
+// };
+// const stack = createNativeStackNavigator<RootStackParamList>();
+
+// const App = (): JSX.Element => {
+//   return (
+//     <>
+//       {/* <View>
+//         <Text>Hello</Text>
+//       </View> */}
+//       <NavigationContainer>
+//         <stack.Navigator>
+//           <stack.Screen
+//             name="Home"
+//             component={Home}
+//             options={{title: 'Trending Products'}}
+//           />
+//           <stack.Screen
+//             name="Details"
+//             component={Details}
+//             options={{title: 'Product Details'}}
+//           />
+//         </stack.Navigator>
+//       </NavigationContainer>
+//     </>
+//   );
+// };
+
+// const styles = StyleSheet.create({});
+
+// export default App;
